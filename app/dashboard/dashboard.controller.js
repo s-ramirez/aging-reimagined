c893492f0cb5798cd971075b246ab007d5331344
Sebastian Ramirez <sramirez10r@gmail.com>
2016-03-14 15:31:22 -0400

    quick fix

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -11,37 +11,37 @@
       {
         color: "#e9b31f",
         url: "/",
-        imageUrl: "/images/twitter.png",
+        imageUrl: "images/twitter.png",
         title: "Community"
       },
       {
         color: "#aade17",
         url: "/",
-        imageUrl: "/images/twitter.png",
+        imageUrl: "images/twitter.png",
         title: "Games"
       },
       {
         color: "#15dcab",
         url: "/",
-        imageUrl: "/images/twitter.png",
+        imageUrl: "images/twitter.png",
         title: "Map"
       },
       {
         color: "#1a80dd",
         url: "/",
-        imageUrl: "/images/twitter.png",
+        imageUrl: "images/twitter.png",
         title: "Gallery"
       },
       {
         color: "#a955e4",
         url: "/",
-        imageUrl: "/images/twitter.png",
+        imageUrl: "images/twitter.png",
         title: "News"
       },
       {
         color: "#e36c53",
         url: "/",
-        imageUrl: "/images/twitter.png",
+        imageUrl: "images/twitter.png",
         title: "Health"
       }
     ];
