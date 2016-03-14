808c9f5a98cc741ed0230f52f568d68274ef50b2
Sebastian Ramirez <sramirez10r@gmail.com>
2016-03-14 15:27:21 -0400

    initial setup of colors and apps

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -7,6 +7,43 @@
 
   function MainController() {
     var vm = this;
-    vm.message = "Hello World";
+    vm.applications = [
+      {
+        color: "#e9b31f",
+        url: "/",
+        imageUrl: "/images/twitter.png",
+        title: "Community"
+      },
+      {
+        color: "#aade17",
+        url: "/",
+        imageUrl: "/images/twitter.png",
+        title: "Games"
+      },
+      {
+        color: "#15dcab",
+        url: "/",
+        imageUrl: "/images/twitter.png",
+        title: "Map"
+      },
+      {
+        color: "#1a80dd",
+        url: "/",
+        imageUrl: "/images/twitter.png",
+        title: "Gallery"
+      },
+      {
+        color: "#a955e4",
+        url: "/",
+        imageUrl: "/images/twitter.png",
+        title: "News"
+      },
+      {
+        color: "#e36c53",
+        url: "/",
+        imageUrl: "/images/twitter.png",
+        title: "Health"
+      }
+    ];
   }
 })();
