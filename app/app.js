f47ecd05771037722c878ca9621d5fd83c6b9e99
Sebastian Ramirez <sramirez10r@gmail.com>
2016-03-14 13:31:04 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app', [
+        'ngTouch',
+        'ngRoute'
+    ]).config(routeConfig);
+
+  function routeConfig($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
+})();
---
+++ app/dashboard/dashboard.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('MainController', MainController);
+
+  function MainController() {
+    var vm = this;
+    vm.message = "Hello";
+  }
+})();
