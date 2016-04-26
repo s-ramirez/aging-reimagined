da3d671a13abecb263c9986e243496714e269406
Sebastian Ramirez <sramirez10r@gmail.com>
2016-04-26 12:29:22 -0400

    preparing for heroku

--- dashboard/models/User.py
+++ dashboard/models/User.py
@@ -6,6 +6,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(32), index = True)
+    name = db.Column(db.String(32))
     password_hash = db.Column(db.String(128))
 
     def hash_password(self, password):
