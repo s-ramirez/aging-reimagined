99f7b5af94e3d7f906a2a62ab4a1ecc9c8e20cb8
Sebastian Ramirez <sramirez10r@gmail.com>
2016-04-26 22:38:20 -0400

    Adding correct icons

--- dashboard/static/app/core/application.service.js
+++ dashboard/static/app/core/application.service.js
@@ -11,42 +11,42 @@
         id: 1,
         color: "#e9b31f",
         url: "http://0.0.0.0:5000/",
-        imageUrl: "static/images/twitter.png",
+        imageUrl: "static/images/community.png",
         title: "Community"
       },
       {
         id: 2,
         color: "#aade17",
         url: "/",
-        imageUrl: "static/images/twitter.png",
-        title: "Games"
+        imageUrl: "static/images/music.png",
+        title: "Music"
       },
       {
         id: 3,
         color: "#15dcab",
         url: "/",
-        imageUrl: "static/images/twitter.png",
+        imageUrl: "static/images/map.png",
         title: "Map"
       },
       {
         id: 4,
         color: "#1a80dd",
         url: "/",
-        imageUrl: "static/images/twitter.png",
+        imageUrl: "static/images/gallery.png",
         title: "Gallery"
       },
       {
         id: 5,
         color: "#a955e4",
         url: "/",
-        imageUrl: "static/images/twitter.png",
+        imageUrl: "static/images/news.png",
         title: "News"
       },
       {
         id: 6,
         color: "#e36c53",
         url: "/",
-        imageUrl: "static/images/twitter.png",
+        imageUrl: "static/images/health.png",
         title: "Health"
       }
     ];
